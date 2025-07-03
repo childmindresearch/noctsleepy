3e7bf23101f766b8aed9c7a271b381b0a6422737
Adam Santorelli <148909356+Asanto32@users.noreply.github.com>
2025-07-03 12:14:11 -0400

    Initial commits

---
+++ src/noctsleepy/io/readers.py
@@ -0,0 +1,56 @@
+"""This module contains the reader function to load processed actigraphy data."""
+
+import pathlib
+
+import polars as pl
+
+
+def read_processed_data(filename: pathlib.Path) -> pl.DataFrame:
+    """Read processed actigraphy data from either csv or parquet files.
+
+    Args:
+        filename: The path to the file.
+
+    Returns:
+        A Polars DataFrame containing the processed data,
+            only the relevant columns containing sleep data are returned.
+
+    Raises:
+        FileNotFoundError: If the file does not exist.
+        ValueError: If the file format is not supported.
+    """
+    if not filename.exists():
+        raise FileNotFoundError(f"The file {filename} does not exist.")
+
+    if filename.suffix == ".csv":
+        processed_data = pl.read_csv(filename, try_parse_dates=True)
+    elif filename.suffix == ".parquet":
+        processed_data = pl.read_parquet(filename)
+    else:
+        raise ValueError(
+            (
+                f"Unsupported file format: {filename.suffix}. "
+                "Supported formats are .csv and .parquet."
+            )
+        )
+
+    return select_data(processed_data)
+
+
+def select_data(processed_data: pl.DataFrame) -> pl.DataFrame:
+    """Select relevant columns from the DataFrame.
+
+    Args:
+        processed_data: The DataFrame to select columns from.
+
+    Returns:
+        A DataFrame with only the relevant columns.
+    """
+    return processed_data.select(
+        [
+            "time",
+            "sleep_status",
+            "sib_periods",
+            "spt_periods",
+        ]
+    )
---
+++ tests/unit/test_readers.py
@@ -0,0 +1,5 @@
+"""Unit tests for the readers module."""
+
+import polars as pl
+
+from noctsleepy.io.readers import read_processed_data, select_data
